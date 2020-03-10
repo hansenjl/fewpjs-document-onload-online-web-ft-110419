0f3f99cdd9efbc6453cb00bbdeb5abf1177e30f5
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-03-10 16:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
+  let par = document.getElementById('text')
+  par.innerHTML = "This is really cool!"
 });
